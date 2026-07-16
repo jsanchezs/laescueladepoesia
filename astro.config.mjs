ae1ef369dd6daef92b764d6ca77b64e3ebef5209
jsanchezs <jesus.sanchez.santamaria@gmail.com>
2026-07-16 23:34:52 +0200

    ci: deploy Astro site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
+  site: "https://jsanchezs.github.io",
+  base: "/laescueladepoesia",
   trailingSlash: "always",
 });
